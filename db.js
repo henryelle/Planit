c2b5a99c3942d6b5745004a768d7a920d46b4270
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-09-29 15:57:17 -0500

    db

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-import { createClient } from '/node_modules/@supabase/supabase-js/dist/main/SupabaseClient.js'
+import { createClient } from '/node_modules/@supabase/supabase-js/'
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://db.jsqcjolulesciklixsfz.supabase.co', "public-sbp_677283778dff4427b82450b999b07274269bf70c-key")
