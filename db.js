831f764ab570de96aaf8f2a30431936b81830dba
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-09-29 15:55:55 -0500

    using API instead of table

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ import { createClient } from '/node_modules/@supabase/supabase-js/dist/main/Supa
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://db.jsqcjolulesciklixsfz.supabase.co', "public-sbp_677283778dff4427b82450b999b07274269bf70c-key")
 
-const { data, error } = await supabase
-.from('test')
-.select()
+// Make a request
+const { data: test, error } = await supabase.from('test').select('*')
+
 
 console.log(data)
