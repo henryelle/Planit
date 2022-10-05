0ca35983acfd44e0a152748934c0368c6690cdec
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-05 17:39:05 -0500

    environment variables

--- scripts/db.js
+++ scripts/db.js
@@ -1,5 +1,5 @@
 import { createClient } from '/../node_modules/@supabase/supabase-js/src/index.ts'
-const supabase = createClient('https://jsqcjolulesciklixsfz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcWNqb2x1bGVzY2lrbGl4c2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQzMTI5MjksImV4cCI6MTk3OTg4ODkyOX0.EFcB75c7Wv1ykOs0K6VQZmDdR334dkVCVPXkenHZeII')
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 let { data: test, error } = await supabase
   .from('test')
   .select('id')
