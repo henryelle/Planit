7fd2128fba60161c55905960a730e51fe891030f
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-05 17:30:25 -0500

    changing import statement

--- scripts/db.js
+++ scripts/db.js
@@ -1,4 +1,4 @@
-import { createClient } from '/../node_modules/@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 const supabase = createClient('https://jsqcjolulesciklixsfz.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcWNqb2x1bGVzY2lrbGl4c2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQzMTI5MjksImV4cCI6MTk3OTg4ODkyOX0.EFcB75c7Wv1ykOs0K6VQZmDdR334dkVCVPXkenHZeII')
 let { data: test, error } = await supabase
   .from('test')
