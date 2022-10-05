d7312c3906acab9c260e5ad3b9b1aa614dc1f65e
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-05 17:44:17 -0500

    file ext

--- scripts/db.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '/../node_modules/@supabase/supabase-js/src/index.ts'
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-let { data: test, error } = await supabase
-  .from('test')
-  .select('id')
-console.log(data)
