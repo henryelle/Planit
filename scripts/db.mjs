792720f4ed424169b1da50dd360f5b42ab4dae96
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-07 13:44:02 -0500

    testing new modules folder

--- scripts/db.mjs
+++ scripts/db.mjs
@@ -1,6 +1,6 @@
-import { createClient } from '/../node_modules/@supabase/supabase-js'
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
-let { data: test, error } = await supabase
-  .from('test')
-  .select('id')
-console.log(data)
+// import { createClient } from '/../node_modules/@supabase/supabase-js'
+// const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+// let { data: test, error } = await supabase
+//   .from('test')
+//   .select('id')
+// console.log(data)
