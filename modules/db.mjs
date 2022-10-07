c3a94af85305b7d072accd127333b9c49ba404d3
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-07 14:04:42 -0500

    supabase object creation

--- modules/db.mjs
+++ modules/db.mjs
@@ -1,15 +1,22 @@
-import { createClient } from '../node_modules/@supabase/supabase-js'
+var SUPABASE_URL = 'https://jsqcjolulesciklixsfz.supabase.co'
+var SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcWNqb2x1bGVzY2lrbGl4c2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQzMTI5MjksImV4cCI6MTk3OTg4ODkyOX0.EFcB75c7Wv1ykOs0K6VQZmDdR334dkVCVPXkenHZeII'
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://jsqcjolulesciklixsfz.supabase.co', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcWNqb2x1bGVzY2lrbGl4c2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQzMTI5MjksImV4cCI6MTk3OTg4ODkyOX0.EFcB75c7Wv1ykOs0K6VQZmDdR334dkVCVPXkenHZeII")
+var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+window.userToken = null
 
-// Make a request
-let { data: test, error } = await supabase
-  .from('test')
-  .select('*')
+// import { createClient } from '../node_modules/@supabase/supabase-js'
 
-export const data = supabase.data;
-console.log(data);
+// // Create a single supabase client for interacting with your database
+// const supabase = createClient('https://jsqcjolulesciklixsfz.supabase.co', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcWNqb2x1bGVzY2lrbGl4c2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQzMTI5MjksImV4cCI6MTk3OTg4ODkyOX0.EFcB75c7Wv1ykOs0K6VQZmDdR334dkVCVPXkenHZeII")
+
+// // Make a request
+// let { data: test, error } = await supabase
+//   .from('test')
+//   .select('*')
+
+// export const data = supabase.data;
+// console.log(data);
 /*
 export const name = 'square';
 
