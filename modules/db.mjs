f7235f3d5405233af6f3f2d969c19312cedcf2a2
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-07 14:15:39 -0500

    once again......

--- modules/db.mjs
+++ modules/db.mjs
@@ -5,7 +5,7 @@
 // var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 // window.userToken = null
 
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '../@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://jsqcjolulesciklixsfz.supabase.co', "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzcWNqb2x1bGVzY2lrbGl4c2Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQzMTI5MjksImV4cCI6MTk3OTg4ODkyOX0.EFcB75c7Wv1ykOs0K6VQZmDdR334dkVCVPXkenHZeII")
