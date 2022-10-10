868845fa29aec585f323da211bd09c02a9047445
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-10 12:33:23 -0500

    testing mysql

---
+++ app.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "199.231.187.232",
+  user: "hosanna",
+  password: "c4pstone"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  con.query("CREATE DATABASE mydb", function (err, result) {
+    if (err) throw err;
+    console.log("Database created");
+  });
+});
