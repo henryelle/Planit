a34b2aa0060bfce1c9ee513431ada0406ada1d16
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-12 12:44:58 -0500

    changingmethod

--- connect_test.php
+++ connect_test.php
@@ -1,4 +1,16 @@
 <?php
-$conn = mysql_connect("199.231.187.232", "hosanna", "gillian") or die(mysql_error() and echo $php_errormsg);
-echo "Connected";
+$dbServerName = "199.231.187.232";
+$dbUsername = "hosanna";
+$dbPassword = "gillian";
+$dbName = "Planit_You";
+
+// create connection
+$conn = new mysqli($dbServerName, $dbUsername, $dbPassword, $dbName);
+
+// check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    $echo "No.";
+}
+echo "Connected successfully";
 ?>
