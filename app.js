dda674bad8d5cd59875a112d264ded42a5647812
Henry Ellebracht <ellebracht.henry@gmail.com>
2022-10-18 16:31:30 -0500

    description on index

--- app.js
+++ app.js
@@ -15,14 +15,14 @@
 //   });
 // });
 
-function showCustomer() {
-  var xhttp;
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-    document.getElementById("main-div").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "connect_test.php", true);
-  xhttp.send();
-}
+// function showCustomer() {
+//   var xhttp;
+//   xhttp = new XMLHttpRequest();
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//     document.getElementById("main-div").innerHTML = this.responseText;
+//     }
+//   };
+//   xhttp.open("GET", "connect_test.php", true);
+//   xhttp.send();
+// }
