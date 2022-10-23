85fa5b72c954db337d32216304291f2b911ef321
Henry EllebrachtPC <lhe522@mail.missouri.edu>
2022-10-23 16:23:56 -0500

    user can select planet

--- scripts/planits.js
+++ scripts/planits.js
@@ -1,7 +1,7 @@
 function createPlanit() {
     let div = document.getElementById("rSide");
-    // TODO APPEND THE IMGPATH WITH USER SELECTED PLANET
-    let imgPath = "'../images/planet.svg'";
+    let planet = document.getElementById("select-planit").value
+    let imgPath = "../images/planets/" + planet.toLowerCase() + ".svg";
     // $('#rSide').append('<div class="col"><img class="planit col" src = '+ imgPath +' /></div>');
     var name = $('#nameInput').val();
     $('#rSide').append('<img class="planit" src = '+ imgPath +' /><p>' + name + '</p>');
