6c9060a0a0995779934d9ab9bc3230e5ec17cd1c
Henry EllebrachtPC <lhe522@mail.missouri.edu>
2022-10-23 16:37:16 -0500

    planit name shows on hover

--- scripts/planits.js
+++ scripts/planits.js
@@ -3,6 +3,9 @@ function createPlanit() {
     let planet = document.getElementById("select-planit").value
     let imgPath = "../images/planets/" + planet.toLowerCase() + ".svg";
     // $('#rSide').append('<div class="col"><img class="planit col" src = '+ imgPath +' /></div>');
-    var name = $('#nameInput').val();
-    $('#rSide').append('<img class="planit" src = '+ imgPath +' /><p>' + name + '</p>');
+    var name = document.getElementById("nameInput").value
+    $('#rSide').append('<a href="#" title="'+name+'"><img class="planit" src = '+ imgPath +' /></a>');
 }
+
+// TODO add call to php file that adds planit to database
+// TODO add call to load user's planits on page load
