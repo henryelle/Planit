b9fe0c813247b894ee412c7c91339395732a6b0b
Hosanna Tesfaye <laluh2011@gmail.com>
2022-10-24 16:38:39 -0500

    register php edits

--- info.php
+++ info.php
@@ -1,7 +1,11 @@
 <?php
-if(isset($_POST['submit']))
-{
+
 //Connect to the Planit_You database on the MySQL server
+$mysqli = new mysqli("199.231.187.232", "hosanna", "gillian", "Planit_You");
+    if($mysqli->connect_error) {
+    exit('Could not connect');
+    }
+// Next, take _POST Variables
     $accountname = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -13,22 +17,18 @@ if(isset($_POST['submit']))
     $password = "gillian";
     $dbname = "Planit_You";
 
-    $mysqli = new mysqli("199.231.187.232", "hosanna", "gillian", "Planit_You");
-    if($mysqli->connect_error) {
-    exit('Could not connect');
-    }
 //Insert new entries (a.k.a. new users) into the users table
 
     $sql = "INSERT INTO users (UserID, AccountName, FirstName, LastName, Password, Email) 
     VALUES ('0', '$accountname', '$firstname', '$lastname', '$password', '$email')";
 
-    if($mysqli->query($sql) === TRUE){
+    $insert = new_mysql_query($mysqli, $sql);
+
+    if($insert){
         echo "A new user has been successfully created!";
     }
     else{
         echo "Error detected! Unable to create user!"
     }
 
-    $mysqli->close();
-}
 ?>
