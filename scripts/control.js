94bf4f7f911e5164b285b4f489c434a75f2b3c3d
Caelen Walker <cm3walker@gmail.com>
2022-10-29 15:34:22 -0500

    selects click date

--- scripts/control.js
+++ scripts/control.js
@@ -10,3 +10,13 @@ function toggleAddMenu() {
       return;
     }
   }
+
+$(document).ready(function(){
+  $('.all-date ul li').click(function(e){
+  $('.all-date ul li').css('backgroundColor', '#050113');
+  $(e.target).css('backgroundColor', '#AE7CFA');
+  $(e.target).css('color','white');
+  let day = $(e.target).text();
+  let month = months.indexOf($("#month-box").text());
+   });
+});
