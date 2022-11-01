2a6d0b76ebcbb18485222afb1fdb4926f723c32e
Hosanna Tesfaye <laluh2011@gmail.com>
2022-11-01 09:58:02 -0500

    app js edits

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-// import mysql from './node_modules/mysql'
+ import mysql from './node_modules/mysql'
 
-// export var con = mysql.createConnection({
-//   host: "199.231.187.232",
-//   user: "hosanna",
-//   password: "*"
-// });
+ export var con = mysql.createConnection({
+   host: "199.231.187.232",
+   user: "hosanna",
+   password: "*"
+ });
 
-// export default con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   con.query("CREATE DATABASE mydb1", function (err, result) {
-//     if (err) throw err;
-//     console.log("Database created");
-//   });
-// });
+ export default con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+   con.query("CREATE DATABASE mydb1", function (err, result) {
+     if (err) throw err;
+     console.log("Database created");
+   });
+ });
 
-// function showCustomer() {
-//   var xhttp;
-//   xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//     document.getElementById("main-div").innerHTML = this.responseText;
-//     }
-//   };
-//   xhttp.open("GET", "connect_test.php", true);
-//   xhttp.send();
-// }
+ function showCustomer() {
+   var xhttp;
+   xhttp = new XMLHttpRequest();
+   xhttp.onreadystatechange = function() {
+     if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("main-div").innerHTML = this.responseText;
+     }
+   };
+   xhttp.open("GET", "connect_test.php", true);
+   xhttp.send();
+ }
