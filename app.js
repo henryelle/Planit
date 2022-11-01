c4da933a424809ab94c37cd69bdff2081e8becf6
Hosanna Tesfaye <laluh2011@gmail.com>
2022-11-01 10:19:12 -0500

    app.js node setup

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-Hosanna
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
@@ -16,33 +15,3 @@ connection.connect((err) =>{
   });
 });
 module.exports = connection;
-
-// import mysql from './node_modules/mysql'
-
-// export var con = mysql.createConnection({
-//   host: "199.231.187.232",
-//   user: "hosanna",
-//   password: "*"
-// });
-
-// export default con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("Connected!");
-//   con.query("CREATE DATABASE mydb1", function (err, result) {
-//     if (err) throw err;
-//     console.log("Database created");
-//   });
-// });
-
-// function showCustomer() {
-//   var xhttp;
-//   xhttp = new XMLHttpRequest();
-//   xhttp.onreadystatechange = function() {
-//     if (this.readyState == 4 && this.status == 200) {
-//     document.getElementById("main-div").innerHTML = this.responseText;
-//     }
-//   };
-//   xhttp.open("GET", "connect_test.php", true);
-//   xhttp.send();
-// }
-main
