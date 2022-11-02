2217b22e493f99591511292d70224678247ee0a5
Hosanna Tesfaye <laluh2011@gmail.com>
2022-11-02 16:57:09 -0500

    app edits

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const mysql = require("mysql");
 const express = require("express")
+const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config({ path: './.env'})
 
 const connection = mysql.createConnection({
-  host: "199.231.187.232",
-  database: "Planit_You",
-  user: "hosanna",
-  password: "gillian"
+  host: '199.231.187.232',
+  database: 'Planit_You',
+  user: 'hosanna',
+  password: 'gillian'
 });
+
 
 connection.connect((err) =>{
   if (err) throw err;
