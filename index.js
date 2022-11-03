5c8dffbc95dd2d9d8024e306a39b67e83edec01d
Hosanna Tesfaye <laluh2011@gmail.com>
2022-11-03 11:41:48 -0500

    edits

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const connection = require('./db');
+const app = express();
+
+const authenticateController=require('./controllers/authenticate-controller');
+const registerController=require('./controllers/register-controller');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.get('/', function (req,res){
+    res.sendFile(__dirname + "/" + "index.html");
+})
+
+app.get('/login.html', function(req,res){
+    res.sendFile(__dirname + "/" + "login.html");
+})
+
+app.post('/api/register', registerController.register);
+app.post('/api/authenticate', authenticateController.authenticate);
+
