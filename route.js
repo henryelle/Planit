3df09e1ad13155e90159d3fdeb919079bdd750c2
Hosanna Tesfaye <laluh2011@gmail.com>
2022-11-07 06:42:27 -0600

    edit ejs view files & create route and controller

---
+++ route.js
@@ -0,0 +1,29 @@
+const router = require("express").Router;
+const {body} = require("express-validator");
+
+const {
+    homePage,
+    login,
+    loginPage,
+    signup,
+    signupPage,
+} = require("./controllers/userController");
+
+const ifNotLoggedIn = (req,res,next) => {
+    if(req.session.userID){
+        return res.redirect('/login');
+    }
+    next();
+}
+
+const ifLoggedIn = (req,res,next) => {
+    if(req.session.userID){
+        return res.redirect('/');
+    }
+    next();
+}
+
+router.get('/', ifNotLoggedIn, homePage);
+
+router.get("/login", ifLoggedIn, loginPage);
+router.post("/login",)
