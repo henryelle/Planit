77f508dfc71f8b307d656e14654dc925882678bd
Hosanna Tesfaye <laluh2011@gmail.com>
2022-11-07 08:38:38 -0600

    edit route.js and index.js

--- route.js
+++ route.js
@@ -26,4 +26,45 @@ const ifLoggedIn = (req,res,next) => {
 router.get('/', ifNotLoggedIn, homePage);
 
 router.get("/login", ifLoggedIn, loginPage);
-router.post("/login", ifLoggedIn)
+router.post("/login", ifLoggedIn,[
+    body("_email", "Invalid email address")
+    .notEmpty()
+    .escape()
+    .trim()
+    .isEmail(),
+    body("_password", "The password length must be at a mininum of 8 characters")
+    .notEmpty()
+    .trim()
+    .isLength({min: 8})
+],
+    login
+);
+router.get("/signup", ifLoggedIn, signupPage);
+router.post("/signup", ifLoggedIn,
+[
+    body("_name", "The name length must be at a mininum of 3 characters")
+    .notEmpty()
+    .escape()
+    .trim()
+    .isLength({min: 3}),
+    body("_email", "Invalid email address")
+    .notEmpty()
+    .escape()
+    .trim()
+    .isEmail(),
+    body("_password", "The password length must be at a mininum of 8 characters")
+    .notEmpty()
+    .trim()
+    .isLength({min: 8})
+],
+    signup
+    );
+
+router.get('/logout', (req,res,next) => {
+    req.session.destroy((err) => {
+        next(err);
+    });
+    res.redirect('/login');
+});
+
+module.exports = router
